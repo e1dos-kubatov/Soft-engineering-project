22e14896f1cb84411f9c4c053c4f1a6d1a69cda3
Eldos-05 <eldos.220505@gmail.com>
2025-10-30 13:16:41 +0600

    Initial commit

---
+++ src/main/java/comsep23/softengineeringscrs/SoftEngineeringScrsApplication.java
@@ -0,0 +1,13 @@
+package comsep23.softengineeringscrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftEngineeringScrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftEngineeringScrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/comsep23/softengineeringscrs/SoftEngineeringScrsApplicationTests.java
@@ -0,0 +1,13 @@
+package comsep23.softengineeringscrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftEngineeringScrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
